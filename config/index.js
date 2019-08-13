f7e5a6468a0082f81dd45f078308f97b9910f2a5
王欢庆 <wanghuanqing@wdeMacBook-Pro.local>
2019-08-13 19:41:38 +0800

    本地地址

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {},
     // Various Dev Server settings
-    host: '192.168.199.210', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8085, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
